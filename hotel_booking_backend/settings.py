59fb740be9cc11184374b9cc715887fd420119ba
oghefav <favouroghenevwoke@gmail.com>
2025-12-09 18:14:57 +0100

    env names and added quotes to all env values

--- hotel_booking_backend/settings.py
+++ hotel_booking_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJ_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', "FALSE").lower() == "true"
@@ -210,6 +210,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 FLW_PUBLIC_KEY = os.getenv('PUBLIC_KEY')
-FLW_SECRET_KEY = os.getenv('SECRET_KEY')
+FLW_SECRET_KEY = os.getenv('FLW_SECRET_KEY')
 FLW_BASE_URL = "https://api.flutterwave.com/v3"
 FLW_ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY')
